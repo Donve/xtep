7e1a4be045b22c4f3c68bec4458f0ac1608c86e3
Donve <461226729.com>
2018-12-28 10:46:09 +0800

    环境搭建完成，开始写项目

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,27 @@ var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('js',function(){
-	gulp.src('./src/js/*.js').pipe(concat('.min')).pipe(uglify())
-	gulp.dest('./dist/js')
+	gulp.src('./src/js/*.js')
+	//.pipe(uglify())
+	.pipe(concat('index.min.js'))
+	.pipe(gulp.dest('./dist/js'))
 })
 gulp.task('sass',function(){
-	gulp.src('./src/sass/*.scss').pipe(sass()).pipe(concat('.min')).pipe(cssnano())
-	gulp.dest('./dist/css')
+	gulp.src('./src/sass/*.scss')
+	.pipe(sass())
+	//.pipe(cssnano())
+	.pipe(rename({"suffix":".min"}))
+	
+	.pipe(gulp.dest('./dist/css'))
 })
-gulp.task('img',function(){
-	gulp.src('./src/img/*.*').pipe(imagemin())
-	gulp.dest('./dist/img')
+gulp.task('imagemin',function(){
+	gulp.src('./src/img/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('./dist/img'))
 })
 gulp.task('default',function(){
 	gulp.watch('./src/js/*.js',['js'])
 	gulp.watch('./src/sass/*.scss',['sass'])
+	gulp.watch('./src/img/*',['img'])
+	
 })
---
+++ src/js/public.js
@@ -0,0 +1,6 @@
+function a(){
+var b=2	;
+var c=3;
+var d=2
+return b+c;
+}
